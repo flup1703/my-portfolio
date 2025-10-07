dd21bef5814d980d8520399e9ce30cb39f891bc2
flup <flup1703@gmail.com>
2025-10-07 12:31:52 +0300

    refactor(mp-19): убрал лишний код.
    Убрал закомиченные проекты, которые были не добавлены.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -7,22 +7,6 @@ export default function Project() {
       <div className="container">
         <h2 className="section-title">Мои проекты</h2>
         <div className="projects-grid">
-          {/* <ProjectCard
-            projectImage={"fas fa-shopping-cart"}
-            image={""}
-            projectName="Интернет-магазин"
-            description="Полнофункциональный магазин с корзиной, фильтрами и поиском"
-            tags={["React", "Redux", "Tailwind"]}
-            href={'Shop'}
-          />
-          <ProjectCard
-            projectImage={"fas fa-comments"}
-            image={""}
-            projectName="Мессенджер"
-            description="Реал-тайм чат с комнатами и уведомлениями"
-            tags={["Vue.js", "Socket.io", "Node.js"]}
-            href={'Chat'}
-          /> */}
           <ProjectCard
             projectImage={"fas fa-tasks"}
             image={process.env.NODE_ENV === 'production' ? "/todo.jpg" : 'todo.jpg'}
@@ -47,14 +31,6 @@ export default function Project() {
             tags={["React", "JavaScript", "Tailwind", "LocalStorage", "API"]}
             href={'LookProfile'}
           />
-          {/* <ProjectCard
-            projectImage={"fas fa-film"}
-            image={""}
-            projectName="Film's"
-            description="Простенький сайт с фильмами"
-            tags={["React", "JavaScript", "Tailwind", "LocalStorage", "API"]}
-            href={'Films'}
-          /> */}
         </div>
       </div>
     </section>
