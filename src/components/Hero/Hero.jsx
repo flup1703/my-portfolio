821295681907f2ab513b6ef95cccef308dd8a77e
flup <flup1703@gmail.com>
2025-12-22 19:06:29 +0300

    feat(mp-36): Добавил снег
    Добавил снег в секцию Hero для новогодней отмосферы
    
    closes issue #36

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
+'use client'
+
 import Button from "../Button/Button";
 import Image from "next/image";
+import Snowfall from 'react-snowfall'
 
 export default function Hero() {
   return (
     <section id="home" className="flex h-[100vh] w-[100vw] text-center items-center justify-center max-[450px]:justify-center max-[770px]:flex-col max-[450px]:w-fit min-[768px]:mt-20 max-[770px]:mt-0">
+      <Snowfall snowflakeCount={300}/>
       <div className="flex flex-col max-w-[800px] gap-4 p-10">
         <h1 className="scroll-m-20 text-center text-7xl font-extrabold tracking-tight text-balance text-(--main-title) whitespace-nowrap animate-fadeInUp max-[450px]:text-4xl max-[1030px]:text-5xl">Frontend Developer</h1>
         <div>
